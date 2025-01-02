3983ecbe8efc46c50022a3dc065c8871c754fc9a
vicsystems <victorasuquob@gmail.com>
2025-01-02 09:52:43 +0100

    added flowbite

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,7 @@
       <div
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
       >
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="https://flowbite.com/docs/images/logo.svg"
             class="h-8"
