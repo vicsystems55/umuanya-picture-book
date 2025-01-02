5729a7a526612fb5ee511838f73599c5d3a6f90e
vicsystems <victorasuquob@gmail.com>
2025-01-02 10:55:21 +0100

    added upload avatar

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,14 @@
           >
         </a>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <router-link to="/register">
+          <!-- <router-link to="/register">
             <button
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Register
             </button>
-          </router-link>
+          </router-link> -->
 
           <!-- <button
             data-collapse-toggle="navbar-cta"
